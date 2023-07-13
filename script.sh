e637efa39e68727dff480778fbb75c7b1cf1cd07
laludonisetiawan <laludonisetiawan@gmail.com>
2023-07-13 12:08:04 +0800

    basic syntax docker compose

---
+++ script.sh
@@ -0,0 +1,11 @@
+# create container
+    docker compose create
+
+# run container
+    docker compose start
+# melihat container 
+    docker compose ps
+# mengengentikan container
+    docker compose stop
+# mengapus container
+    docker compose down
